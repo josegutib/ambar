f3a617e1bd273ae75251a374d91a51bb30f27929
josefo <josemagut@hotmail.com.ar>
2019-07-10 16:14:33 -0300

    galeria terminada y ajuste del margen del contenido

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,95 @@ const App = (function() {
       id: 'product-squid',
       name: 'Squid',
       img: 'assets/squid.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, distinctio.'
+      // description: ['Species: Molusco', 'Origin:','Fish Method:','Capture Season:', 'Presentation:','Elaboration:']
+      description: [
+        {
+          categoria: 'Species',
+          valor: 'Cephalopods.'
+        },
+        {
+          categoria: 'Origin',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Fish Method',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Capturer Season',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Presentation',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Elaboration',
+          valor: 'Lorem ipsum dolor.'
+        },
+      ]
     },
     {
       id: 'product-shrimp',
       name: 'Shrimp',
       img: 'assets/shrimp.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, distinctio.'
+      description: [
+        {
+          categoria: 'Species',
+          valor: 'Crustaceans.'
+        },
+        {
+          categoria: 'Origin',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Fish Method',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Capturer Season',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Presentation',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Elaboration',
+          valor: 'Lorem ipsum dolor.'
+        },
+      ]
     },
     {
       id: 'product-seabass',
       name: 'Seabass',
       img: 'assets/seabass.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, distinctio.'
+      description: [
+        {
+          categoria: 'Species',
+          valor: 'Serranidae.'
+        },
+        {
+          categoria: 'Origin',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Fish Method',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Capturer Season',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Presentation',
+          valor: 'Lorem ipsum dolor.'
+        },
+        {
+          categoria: 'Elaboration',
+          valor: 'Lorem ipsum dolor.'
+        },
+      ]
     }
   ]
 
@@ -40,9 +116,19 @@ const App = (function() {
       <figure id='${props.id}' class="image is-hoverable-btn is-clickable is-animated-fast">
         <img src="${props.img}" alt=""></img>
       </figure>
-      <h2 class="subtitle has-text-white desc">${props.name}</h2>
+      <h2 class="subtitle has-text-white desc has-text-centered">${props.name}</h2>
     </div>
   `)
+
+  const ProductDescription = props => {
+    let descriptionStrEl = '';
+    props.description.forEach(function(el, i){
+      const descriptionLiEl = `<li><span class="has-text-weight-semibold">${el.categoria}:</span> ${el.valor}</li>`;
+      descriptionStrEl += descriptionLiEl
+    })
+
+    return `<ul>${descriptionStrEl}</ul>`
+  }
 
   function populate() {
     productColumnsEl.innerHTML = products.map(Product).join('\n')
@@ -73,8 +159,9 @@ const App = (function() {
   function openModal(product){
     modalProductImgEl.setAttribute('src', product.img)
     modalProductNameEl.innerHTML = product.name
-    modalProductDescriptionEl.innerHTML = product.description
+    modalProductDescriptionEl.innerHTML = ProductDescription({description:product.description})
     modalEl.classList.add('is-active')
+
   }
 
   function closeModal(){
